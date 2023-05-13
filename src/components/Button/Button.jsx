397c7833986de608476baf8d82ea4eefca12c73e
Mikhailo Salko <slipknotmisha@gmail.com>
2023-05-13 13:14:55 +0300

    add Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types'
+import styles from './Button.module.css';
+
+const Button = () => {
+  return (
+    <button type="submit" className={styles.button}>
+      Load more
+    </button>
+  );
+};
+export default Button;
