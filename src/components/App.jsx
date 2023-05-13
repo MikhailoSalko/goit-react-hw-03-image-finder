e02cb1585a898513e4a04dd69782b22811b09db0
Mikhailo Salko <slipknotmisha@gmail.com>
2023-05-13 13:23:45 +0300

    update base styles and turn App into class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+// import Loader from './Loader/Loader';
+// import ImageGallery from './ImageGallery/ImageGallery';
+// import Modal from './Modal/Modal';
+// import Button from './Button/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100px',
+          fontSize: 40,
+          color: '#010101',
+          display: 'grid',
+          gridTemplateColumns: '1fr',
+          gridGap: '16px',
+          paddingBottom: '24px',
+        }}
+      >
+        <Searchbar />
+        {/* <ImageGallery /> */}
+        {/* <Modal /> */}
+        {/* <Button /> */}
+        {/* <Loader /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
